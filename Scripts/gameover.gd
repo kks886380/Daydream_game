ab9cf683c24e7a251b3e8cb5df9014c27ff02f11
kks886380 <kks886380@gmail.com>
2025-09-28 13:41:57 +0800

    final_version

--- Scripts/gameover.gd
+++ Scripts/gameover.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	if Global.score < 114:
 		Dialogic.start("bedend")
 	elif Global.score >= 114 and Global.score <= 514:
-		Dialogic.start("specilend")
+		Dialogic.start("specialendd")
 	else :
 		Dialogic.start("goodend")
 	text = "最終分數: " + str(Global.score)
